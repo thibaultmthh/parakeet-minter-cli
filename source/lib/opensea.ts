854d029fa44cbd55632c047f9f0081b89fa4e70c
Thibault mathian <thibault.mathiant@free.fr>
2022-03-24 10:14:19 +0100

    x

--- source/lib/opensea.ts
+++ source/lib/opensea.ts
@@ -1,7 +1,7 @@
 import { isTestnet } from "../constantes";
 import { web3 } from "./web3";
 import fs from "fs";
-console.log(__dirname, "webhooks.ts");
+console.log(process.cwd(), "webhooks.ts");
 
 const abi = JSON.parse(fs.readFileSync("data/abi/opensea.json", "utf8"));
 
